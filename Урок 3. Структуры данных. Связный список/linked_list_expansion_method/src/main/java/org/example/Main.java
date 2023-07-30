446db4f9098f34daa971636dcd3b7c3804c79ae5
Igor <igorjordan210@gmail.com>
2023-07-30 23:59:07 +0300

    Add home 2

---
+++ Урок 3. Структуры данных. Связный список/linked_list_expansion_method/src/main/java/org/example/Main.java
@@ -0,0 +1,74 @@
+package org.example;
+
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addNode(1);
+        list.addNode(2);
+        list.addNode(3);
+        list.addNode(4);
+
+        System.out.println("Original list:");
+        list.printList();
+
+        list.reverse();
+
+        System.out.println("\nReversed list:");
+        list.printList();
+    }
+}
